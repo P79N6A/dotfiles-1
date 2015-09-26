564dbb70a61c6fe25fb697385f19c156fca0ee47
levi <ge4los@gmail.com>
2015-09-26 11:54:53 +0800

    fix mkdotfile

--- mkdotfile.sh
+++ mkdotfile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-#link dotfiles from dropbox to ~
+#link dotfiles from pwd to ~
 
 local=`pwd`
 
@@ -8,16 +7,14 @@ local=`pwd`
 #mklink linkfile target
 
 #ln -sf $(pwd)/.zshrc ~/.zshrc
-#ln -sf $(pwd)/.alias ~/.alias
 #ln -sf $(pwd)/.vimrc ~/.vimrc
-ln -sF $(pwd)/.vimrc.local ~/.vimrc.local
-ln -sF $(pwd)/.vimrc.before.local ~/.vimrc.before.local
-ln -sF $(pwd)/.vimrc.bundles.local ~/.vimrc.bundles.local
-ln -sF $(pwd)/.vimperatorrc ~/.vimperatorrc
+#ln -sf $(pwd)/.aliase<Plug>(neocomplete_fallbac
+ln -sf $(pwd)/.vimrc.local ~/.vimrc.local
+ln -sf $(pwd)/.vimrc.before.local ~/.vimrc.before.local
+ln -sf $(pwd)/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/.aliases ~/.aliases
+#ln -sF $(pwd)/.vimperatorrc ~/.vimperatorrc
 #ln -sFd $(pwd)/UltiSnips ~/.vim/UltiSnips
-
-#ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
-
 #ln -sf $(pwd)/.bashrc ~/.bashrc
-
 #ln -sf $(pwd)/.kermrc ~/.kermrc
